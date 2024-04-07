0b10141a279f83805e73bdee3c4281d0122f40c2
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-04-07 11:08:43 -0300

    add: aula01

---
+++ aula01 - introducao/conceitos-iniciais.R
@@ -0,0 +1,68 @@
+print("Hello World!")
+
+# VARIÁVEIS E ATRIBUIÇÃO #
+a = 10
+# complete
+
+b <- 9
+#complete
+
+nome = "Eduardo"
+# complete
+
+# TIPOS DE DADOS (pt1) #
+
+# String (texto)
+a = "Breno"
+b = "A melhor chuteira não te faz o melhor jogador" 
+
+# Inteiro (int)
+a = 1981
+b = -12
+
+# Ponto Flutuante (float)
+a = 10.5
+b = 3.1415
+c = 503.54
+
+# Booleano (bool)
+a = TRUE
+b = FALSE
+
+# OPERACOES ARITMETICAS #
+
+# Soma (+)
+a = 5
+b = 2
+
+c = a + b
+
+# Subtração (-)
+a = 25
+b = 10
+
+# complete
+
+# Produto (*)
+a = 2.5
+b = 2
+
+c = a * b
+
+# Divisao (/)
+a = 56
+b = 10
+
+c = a / b
+
+# Potenciacao (^)
+a = 2
+b = 4
+
+c = a ^ b
+
+# Logaritmo (log)
+a = 1000
+b = 10
+
+c = log(a, b)
